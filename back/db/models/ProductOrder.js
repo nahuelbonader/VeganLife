ca251034efe872ffdd4941790e434575648fc2e4
Maxi Pugliese <maxi.pugliese@gmail.com>
2020-11-19 14:41:00 -0300

    Modelos Cart/Category/PO/User

---
+++ back/db/models/ProductOrder.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//Creacion del Schema ProductOrder
+
+const productOrderSchema = new Schema({
+    product: {type: Schema.Types.ObjectId, ref: "product"},
+    units: Number  
+});
+
+
+// Creacion del modelo ProductOrder
+const ProductOrder = mongoose.model('productOrder', productOrderSchema);
+
+// Exportar el modelo
+
+    module.exports = ProductOrder
