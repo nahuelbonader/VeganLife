273a3bab19908ca5c3d8e813051745d490feb775
Nahuel <bonadernahuel@hotmail.com>
2020-11-26 12:02:00 -0300

    cambio nombre SingleRecipe

--- front/App.js
+++ front/App.js
@@ -3,7 +3,7 @@ import { createStackNavigator } from "react-navigation-stack";
 import FeedRecetas from "./src/screens/FeedRecetaScreen";
 import Login from "./src/screens/LoginScreen";
 import Register from "./src/screens/RegisterScreen";
-import Recipe from "./src/screens/SingleRecipe";
+import Recipe from "./src/screens/SingleRecipeScreen";
 
 const navigator = createStackNavigator(
   {
