1b7cb3cbef7bcf7728fe079beac585cb62d3c3da
Maxi Pugliese <maxi.pugliese@gmail.com>
2020-11-26 21:48:37 -0300

    console

--- front/src/screens/FeedRecetaScreen.js
+++ front/src/screens/FeedRecetaScreen.js
@@ -13,9 +13,6 @@ const FeedRecetaContainer = () => {
   const dispatch = useDispatch();
   const randomRecipe = useSelector((state) => state.randomRecipe.randomRecipe);
 
-console.log(randomRecipe[0]._id)
-
-
   useEffect(() => {
     axios
       .get(`http://${IP}:1337/api/categories`)
