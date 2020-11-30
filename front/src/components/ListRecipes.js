9768e034d4fb8298a0e9bd4f81da30d5dce88453
Maxi Pugliese <maxi.pugliese@gmail.com>
2020-11-30 17:43:30 -0300

    ListRecipeStyles

--- front/src/components/ListRecipes.js
+++ front/src/components/ListRecipes.js
@@ -1,8 +1,6 @@
 import React from "react";
-import { View, Text, FlatList, TouchableOpacity, Image } from "react-native";
+import { View, Text, FlatList, TouchableOpacity, Image, StyleSheet, ImageBackground } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import { ScrollView } from "react-native-gesture-handler";
-import styles from "../styles/carouselFeed"
 
 const Recipe = ({ recipes }) => {
   const navigation = useNavigation();
@@ -10,29 +8,53 @@ const Recipe = ({ recipes }) => {
 
 
   return (
-    <View style={{width: 1000}}>
-      <Text >TODAS LAS RECETAS</Text>
       <FlatList
-        
+        contentContainerStyle={styles.container}
         data={recipes}
         renderItem={({ item }) => (
           <TouchableOpacity
             onPress={() =>
-              navigation.navigate("Recipe", { categoryId: item._id })
+              navigation.navigate("Recipe", { recipeId: item._id })
             }
           >
-            <Image style={{width: 200, height:200}} source={{ uri: item.image }}></Image>
-            <Text>{item.title}</Text>
+            <ImageBackground style={styles.image} imageStyle={styles.border} source={{ uri: item.image }}>
+              <Text style={styles.text}>{item.title}</Text>
+              </ImageBackground>
+            
           </TouchableOpacity>
         )}
         keyExtractor={(recipes) => recipes._id}
-        //numColumns={2}
         showsHorizontalScrollIndicator={false}
       >
 
       </FlatList>
-    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    
+  },
+  text: {
+    fontSize: 20,
+    color: "white",
+    textAlign: "center",
+    justifyContent: "center",
+    fontWeight: "bold",
+    marginTop:15,
+    padding: 60 
+
+  },
+  image: {
+    width: 350,
+    height: 220,
+    marginTop: 15,
+  },
+  border: {
+    borderRadius: 10
+  }
+});
+
 
 export default Recipe;
