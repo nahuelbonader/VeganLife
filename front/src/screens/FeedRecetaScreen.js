98de8e159c69cccb23ab73777d4fb7b7c64fa6fc
DaniBordoli <d.c.bordoli@gmail.com>
2020-11-30 20:21:31 -0300

    dani intento push 2

--- front/src/screens/FeedRecetaScreen.js
+++ front/src/screens/FeedRecetaScreen.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { Text, StyleSheet, View, Button } from "react-native";
+import { Text, StyleSheet, View, Button, ScrollView } from "react-native";
 import Categories from "../components/Categories";
 import CarouselFeed from "../components/CarouselFeed";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchRandomRecipe } from '../actions/recetas'
 import IP from "../../env";
+import Recipe from '../components/ListRecipes'
 
 
 
@@ -15,30 +16,28 @@ const FeedRecetaContainer = ({navigation}) => {
   const dispatch = useDispatch();
   const randomRecipe = useSelector((state) => state.randomRecipe.randomRecipe);
 
-
-
-
   useEffect(() => {
     axios
       .get(`http://${IP}:1337/api/categories`)
       .then((res) => setCategorias(res.data));
-    dispatch(fetchRandomRecipe())
+      dispatch(fetchRandomRecipe())
   }, []);
 
 
 
   return (
-    <>
+    <ScrollView style={styles.container}>
       <Button title='profile' onPress={()=>navigation.navigate('Profile')}></Button>
       <CarouselFeed randomRecipe={randomRecipe}/>
       <Categories categorias={categorias} />
-    </>
+      <Recipe recipes={randomRecipe}/>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 30,
+  container: {
+    flex: 1,
   },
 });
 
