308a1bf0bb1ee84b81c26b7043047f7db82680e2
Nahuel <bonadernahuel@hotmail.com>
2020-12-03 17:38:52 -0300

    avances en Profile-VC + login/register

---
+++ front/src/utils/constants.js
@@ -0,0 +1,5 @@
+export const recipeImg =
+  "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngegg.com%2Fes%2Fpng-yjdnk&psig=AOvVaw2ml9YNEFL3_kZ9ndKhChxB&ust=1606530297851000&source=images&cd=vfe&ved=2ahUKEwiGnojv1aHtAhVNANQKHUZXCmgQjRx6BAgAEAc";
+
+export const ownerImg =
+  "https://png.pngtree.com/png-vector/20191009/ourmid/pngtree-user-icon-png-image_1796659.jpg";
