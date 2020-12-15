665ac1cd2e9b9d077b596085e3195d372bf86e23
Maxi Pugliese <maxi.pugliese@gmail.com>
2020-12-15 19:28:27 -0300

    Feed Comercios y Single Comercio SIN ESTILOS

--- front/src/screens/FeedStack.js
+++ front/src/screens/FeedStack.js
@@ -12,7 +12,7 @@ import Splash from '../components/Splash'
 import MyStores from "./MyCommercesScreen";
 import CommercePanel from "./CommercePanelScreen";
 import SingleProduct from "./SingleProductScreen";
-
+import SingleMarket from "../components/SingleMarket"
 
 const Stack = createStackNavigator();
 
@@ -38,6 +38,7 @@ const FeedStack = () => {
       <Stack.Screen name="MyCommerces" component={MyStores} />
       <Stack.Screen name="CommercePanel" component={CommercePanel} />
       <Stack.Screen name="SingleProduct" component={SingleProduct} />
+      <Stack.Screen name="SingleMarket" component={SingleMarket} />
     </Stack.Navigator>
   );
 };
